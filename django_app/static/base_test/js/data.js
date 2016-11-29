d86f17d5279b1ce54eb8534fcaeece60137a4fba
박상준 <gggttteee3@gmail.com>
2016-11-29 13:28:08 +0900

    Adding google API

---
+++ django_app/static/base_test/js/data.js
@@ -0,0 +1,5 @@
+friends_callback({"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[37.515836,127.020132]},"id":"usc000csx3"},
+{"type":"Feature","geometry":{"type":"Point","coordinates":[37.517932,127.021830]},"id":"ak10559055"},
+{"type":"Feature","geometry":{"type":"Point","coordinates":[37.515366,127.024900]},"id":"ak10559055"},
+{"type":"Feature","geometry":{"type":"Point","coordinates":[37.5107224,127.033926]},"id":"ak10559055"},
+{"type":"Feature","geometry":{"type":"Point","coordinates":[37.515688,127.021456]},"id":"ak10559055"}]});
